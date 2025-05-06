522d594f43d68e6dfc94d86c7a7145876012125d
ce7er2s <spike.calhun@yandex.ru>
2025-05-06 17:01:23 +0300

    Implement basic Competitor class functionality

--- pkg/competitor/competitor.go
+++ pkg/competitor/competitor.go
@@ -1,6 +1,30 @@
 package competitor
 
+import (
+	"fmt"
+	"slices"
+	"time"
+)
+
 type Competitor struct {
-	status       string
+	Status       CompetitorStatusType
 	CompetitorID int
+	StartTime    time.Time
+}
+
+func NewCompetitor(id int, startTime time.Time) Competitor {
+	return Competitor{
+		Status:       STATUS_REGISTERED,
+		CompetitorID: id,
+		StartTime:    startTime,
+	}
+}
+
+func (c *Competitor) SetStatus(status CompetitorStatusType) error {
+	if slices.Contains(competitorFSM[c.Status], status) {
+		c.Status = status
+		return nil
+	} else {
+		return fmt.Errorf("Can't change state from %s to %s", competitorStatusToString[c.Status], competitorStatusToString[status])
+	}
 }
